336fe40ff14be740d2f7588d4acfbd132275a634
Daniel <danielspradah0@gmail.com>
2022-11-13 16:41:50 -0500

    Connect buttons to constants

---
+++ assets/scripts/script.js
@@ -0,0 +1,44 @@
+const display = document.getElementById("screen")
+
+const addBtn = document.getElementById("addBtn");
+const subBtn = document.getElementById("subBtn");
+const multiplyBtn = document.getElementById("multiplyBtn");
+const divideBtn = document.getElementById("divideBtn");
+const equalBtn = document.getElementById("equalBtn");
+
+
+const oneBtn = document.getElementById("1");
+const twoBtn = document.getElementById("2");
+const threeBtn = document.getElementById("3");
+const fourBtn = document.getElementById("4");
+const fiveBtn = document.getElementById("5");
+const sixBtn = document.getElementById("6");
+const sevenBtn = document.getElementById("7");
+const nineBtn = document.getElementById("8");
+const tenBtn = document.getElementById("9");
+const zeroBtn = document.getElementById("0");
+const dotBtn = document.getElementById(".");
+
+const acBtn = document.getElementById("");
+const delBtn = document.getElementById("");
+
+
+
+
+
+addBtn.addEventListener('click', () => {
+    //math
+})
+
+addBtn.addEventListener('click', () => {
+    //math
+})
+
+addBtn.addEventListener('click', () => {
+    //math
+})
+
+addBtn.addEventListener('click', () => {
+    //math
+})
+
